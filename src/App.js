d4946fdba9010e4ac7fa56476a3417b203fd28e7
Omar <mandragom@gmail.com>
2022-11-27 00:13:18 -0600

    Add Consume REST API

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
-import React, { Component } from 'react';
-import UserInfo from './componentes/Userinfo'; 
+import React, { useState } from 'react';
+import axios from'axios';
 
-class App extends Component {
-  state = {
-    _mail: '',
-    _name: '',
-    _firstusername: '',
-    _secondusername: ''
+
+function App() {
+  const mailRef = useState();  
+  const nameRef = useState();  
+  const firstsurnameRef = useState();  
+  const secondsurnameRef = useState();   
+  const[varfiles, setFiles]=useState();
+
+  const UploadFile=e=>{
+    setFiles(e);
   }
 
-  SearchApi = () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 'mail': this.state._mail, 'name': this.state._name, 'firstsurname': this.state._firstusername, 'secondsurname': this.state._secondusername })
-    };
 
-    const url = "http://localhost:3001/api/v1/step";
+  const submitform = async() => {
 
-    fetch(url, requestOptions)
-      .then(response => response.text())
-      .then((response) => {
-        console.log(response)
-      })
-    . catch(err => console.log(err))
-  }
+    //add data
+    const f = new FormData();
+    f.append("correo", mailRef.current.value);
+    f.append("nombre", nameRef.current.value);
+    f.append("primerapellido", firstsurnameRef.current.value);
+    f.append("segundoapellido", secondsurnameRef.current.value);
+    f.append("file", varfiles[0]);
 
-  userdata = (mail, name, firstusername, secondusername) => {
-    this.setState({
-      _mail: mail,
-      _name: name,
-      _firstusername: firstusername,
-      _secondusername: secondusername
-    },
-    () => {
-      this.SearchApi();
-    }
-    )
-  }
-  
-  render(){
-    return (
-      <div className="App">
-        <UserInfo Info={this.userdata} />
+    await axios.post("http://localhost:3001/api/v1/step", f, {headers: {'Content-Type': 'multipart/form-data'}})
+    .then(response=>{
+      console.log(response.data);
+    }).catch(error=>{
+      console.log(error);
+    });
+
+  };
+
+  return(
+    <div style={{"padding":"15px"}}>
+      {<img className="d-flex flex-row-reverse" src='https://ieomsociety.org/ieom/wp-content/uploads/2022/05/TEC-logo-300x79.png'/> }
+
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+            <label className="form-label col d-flex flex-row">Correo</label>
+            <input ref={mailRef} type="text" className="form-control col" id="txtmail"/>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+            <label className="form-label col d-flex flex-row">Nombre</label>
+            <input ref={nameRef} type="text" className="form-control col" id="txtname"/>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+            <label className="form-label col d-flex flex-row">Primer Apellido</label>
+            <input ref={firstsurnameRef} type="text" className="form-control col" id="txtfirstsurname"/>
+        </div>
+
+        <div className="col-xl-6 col-lg-6 col-md-6 col-xs-12">
+            <label className="form-label col d-flex flex-row">Segundo Apellido</label>
+            <input ref={secondsurnameRef} type="text" className="form-control col" id="txtsecondsurname"/>
+        </div>
+
+        <div className="col-xl-12 col-lg-12 col-md-12 col-xs-12" style={{"padding-top": "15px"}}>
+            <input type="file" name="file" className="form-control col" onChange={(e)=>UploadFile(e.target.files)}/>
+        </div>
+    </div>
+
+      
+      <div className="row" style={{"padding-top": "20px"}}>
+          <div className="d-flex flex-row-reverse">
+              <button className="btn btn-primary" onClick={submitform}>Guardar</button>
+          </div>
       </div>
-    );  
-  }
+
+    </div>
+  )
 }
 
 export default App;
